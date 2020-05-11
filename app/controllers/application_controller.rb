89471603255721403d60ba81a380c6703cca37e5
Matt Frazier <frazier3388@yahoo.com>
2020-05-12 03:22:23 +0900

    users controller with index and show actions and views, add name fields to user, routes for users after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def after_sign_in_path_for(resources)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resources)
+    user_session_path
+  end
 end
